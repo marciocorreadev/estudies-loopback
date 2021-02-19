26752dab99804cc953bfabd6d27eabbe268b77f8
Márcio Corrêa <msc@outlook.com.br>
2021-02-18 22:52:43 -0300

    Estudando loopback

--- common/models/produto.js
+++ common/models/produto.js
@@ -1,5 +1,32 @@
 'use strict';
 
 module.exports = function (Produto) {
+    Produto.testando = function (ctx, body, callback) {
+        console.log(ctx)
+        callback()
+    }
+
+    Produto.remoteMethod('testando', {
+        description: 'Teste de interceptação de API',
+        http: { path: '/testando', verb: 'get' },
+        accepts: [
+            {
+                arg: 'contexto',
+                type: 'Object',
+                http: {
+                    source: 'context',
+                },
+            },
+            {
+                arg: 'body',
+                type: 'object',
+                required: true,
+                http: {
+                    source: 'body',
+                },
+            },
+        ]
+    }
+    );
 
 };
